13397d5a84f94ddbcbc6c2f742af2844d8e53fcb
Alien <lmllewellyn@gmail.com>
2024-08-04 20:26:15 -0400

    fixed model columns to READMEs standard

--- models/Product.js
+++ models/Product.js
@@ -23,19 +23,25 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       references : {
         model: 'category',
         key: 'id',
         unique: false
-      }
+      },
     },
   },
   {
